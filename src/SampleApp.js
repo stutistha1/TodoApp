8deb6d562c5f667996a7b9c05c1e152eb05df4c8
stutistha1 <shrestha.stuti@gmail.com>
2022-03-02 17:45:51 +0545

    Persist

--- src/SampleApp.js
+++ src/SampleApp.js
@@ -4,17 +4,17 @@ import logger from 'redux-logger';
 import {createStore, applyMiddleware} from 'redux';
 import Todo from './component/Todo';
 import rootReducer from './reducers/rootReducer';
-// import {PersistGate} from 'redux-persist/integration/react';
-// import {persistStore} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
 
 const store = createStore(rootReducer, applyMiddleware(logger));
-// const persistor = persistStore(store);
+const persistor = persistStore(store);
 
 const SampleApp = () => (
   <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    <Todo />
-    {/* </PersistGate> */}
+    <PersistGate loading={null} persistor={persistor}>
+      <Todo />
+    </PersistGate>
   </Provider>
 );
 
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,15 @@
 import {combineReducers} from 'redux';
 import todoReducer from './todoReducer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer} from 'redux-persist';
 
-const rootReducer = combineReducers({todos: todoReducer});
+const rootReducer = combineReducers({
+  todos: persistReducer(persistConfig, todoReducer),
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 
 export default rootReducer;
